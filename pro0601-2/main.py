5f5c515043b0611461ae1ed33d89a3761fa06752
Parkdoheum <microform@korea.com>
2020-06-01 14:04:30 +0900

    '0204'

--- pro0601-2/main.py
+++ pro0601-2/main.py
@@ -15,39 +15,13 @@ def home():
    </html>
    '''
 
-
-@app.route('/login1/<name>')
-def login1(name):
-   return 'welcome %s' % (name)
-
-
 @app.route('/login2')
-def login2():
-   name = request.args.get('name');
-   pwd = request.args.get('password');
-   return 'welcome %s, %s' % (name, pwd)
-
-
-@app.route('/login3')
 def login3():
    name = request.args.get('name');
    pwd = request.args.get('password');   
    return '<html><body>welcome %s, %s</body></html>' % (name, pwd)
    
 
-
-@app.route('/success/<name>')
-def success(name):
-   return 'welcome %s' % name
-
-@app.route('/login',methods = ['POST', 'GET'])
-def login():
-   if request.method == 'POST':
-      user = request.form['nm']
-      return redirect(url_for('success',name = user))
-   else:
-      user = request.args.get('nm')
-      return redirect(url_for('success',name = user))
 
 if __name__ == '__main__':
    app.run(debug = True)
