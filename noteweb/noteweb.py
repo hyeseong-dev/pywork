0a64a79eee89c14a3cacc80228971d4436cc3686
hkit302 <hkit302@hkit.kr>
2020-06-03 11:15:48 +0900

    a

--- noteweb/noteweb.py
+++ noteweb/noteweb.py
@@ -53,6 +53,14 @@ def setNote():
 
     return redirect("/noteList")
 
+@app.route("/delNote")
+def delNote():
+    index = request.args.get("index")
+    # memos.remove(int(index))
+    del memos[int(index)]
+    return redirect("/noteList")
+
+
 
 
 if __name__ == '__main__':
